7e463387057e39efb4d3e5c0714321f5d2eea08e
Cheekyfunkymonkey <shigechu@gmail.com>
2019-04-11 12:24:02 +1200

    Create math.js and index.js

---
+++ excercise/index.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+
+console.log("math.add(1, 2)", math.add(1, 2));
+console.log("math.add(1, 2)", math.sub(3, 4));
+console.log("math.add(1, 2)", math.mul(5, 6));
+console.log("math.add(1, 2)", math.div(7, 8));
